014d69b9771d7924cb48443ec1d4e3923a6bbffb
Jonathan1700 <jonatha06castro@gmail.com>
2026-04-19 13:35:27 -0500

    Organización de carpetas y limpieza de duplicados

--- POO/DECORADORES/ejemplo1.py
+++
@@ -1,5 +0,0 @@
-# uso del args 
-def revision (*args):
-    print(f"contenido: {args}")
-revision ("toalla", "agua")
-# todo lo que se guarda en args se convierte en tuplas 
--- POO/DECORADORES/ejemplo2.py
+++
@@ -1,6 +0,0 @@
-def pedido (**kwargs):
-    print (f"contenido:{kwargs}")
-
-pedido (comida = "salchipapa", salsa = "salsa de tomate", proteina = "pollo")
-
-# todo lo que le damos al decorador **kwargs se convierte en un diccionario 
